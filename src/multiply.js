d3542da72897585391993e8b2f47b5def48467c3
Guilherme Weissheimer <guilherme.w@universo.univates.br>
2023-03-11 23:18:36 -0300

    feat: Add new lines to break sonar

--- src/multiply.js
+++ src/multiply.js
@@ -1,3 +1,29 @@
-const multiply = (a, b) => a * b;
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+const multiply3 = (a, b) => {
+  return a * b;
+};
+
+const multiply4 = (a, b) => {
+  return a * b;
+};
+
+const multiply5 = (a, b) => {
+  return a * b;
+};
+
+const multiply6 = (a, b) => {
+  return a * b;
+};
+
+const multiply7 = (a, b) => {
+  return a * b;
+};
 
 module.exports = multiply;
