105014800d32b723137ee13545dad192058a6cf0
Guilherme Weissheimer <guilherme.w@universo.univates.br>
2023-03-18 22:07:01 -0300

    feat: Add empty test to break pipeline

--- src/division.js
+++ src/division.js
@@ -2,24 +2,4 @@ const division = (a, b) => {
   return a / b;
 }
 
-const division2 = (a, b) => {
-  return a / b;
-}
-
-const division3 = (a, b) => {
-  return a / b;
-}
-
-const division4 = (a, b) => {
-  return a / b;
-}
-
-const division5 = (a, b) => {
-  return a / b;
-}
-
-const division6 = (a, b) => {
-  return a / b;
-}
-
 module.exports = division;
---
+++ src/division.test.js
@@ -0,0 +1,5 @@
+// const division = require('./division');
+
+// test('divide 4 by 2 to equal 2', () => {
+//   expect(division(4, 2)).toBe(2);
+// });
